f8c3a87c213d39d8150dab364adc9904c78292c6
Lefel95 <felipe.pbgomes@gmail.com>
2023-12-15 19:22:40 -0300

    chore: removes all js from repository

--- src/infra/server.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import app from "./app";
-
-const port = process.env.API_PORT || "3001";
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-
