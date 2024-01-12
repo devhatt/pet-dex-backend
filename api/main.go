777f52d7fdaf0058c8c8c8a06697ee20d6015a8f
Luan A. <barkon32@tuta.io>
2024-01-12 11:30:33 -0300

    renamed and moved ong controller to main.go

--- api/main.go
+++ api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	ongcontroller "pet-dex-backend/v2/api/controllers/ong"
 	petcontroller "pet-dex-backend/v2/api/controllers/pet"
 	"pet-dex-backend/v2/api/routes"
 	"pet-dex-backend/v2/infra/config"
@@ -19,16 +20,20 @@ func main() {
 
 	database := config.InitConfigs()
 	dbPetRepo := db.NewPetRepository(database)
+	dbOngRepo := db.NewOngRepository(database)
 
 	exampleUseCase := usecase.NewExampleUseCase(dbPetRepo)
 	findPetUseCase := usecase.NewPetUseCase(dbPetRepo)
+	createOngUseCase := usecase.NewOngUseCase(dbOngRepo)
 
 	exampleController := petcontroller.NewExampleController(exampleUseCase)
 	findPetController := petcontroller.NewFindPetController(findPetUseCase)
+	createOngController := ongcontroller.NewCreateOngcontroller(createOngUseCase)
 
 	contrllers := routes.Controllers{
-		FindPetController: findPetController,
-		ExampleController: exampleController,
+		FindPetController:   findPetController,
+		ExampleController:   exampleController,
+		CreateOngController: createOngController,
 	}
 
 	router := routes.InitializeRouter(contrllers)
