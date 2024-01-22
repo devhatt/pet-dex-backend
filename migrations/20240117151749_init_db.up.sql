060bc31be102a2ab354a44f3d6e07200a94071b6
Kevin Gama <kevin.bg@outlook.com>
2024-01-22 20:27:19 -0300

    fix(database): ajuste na migration init_db adicionando FK de pets para breeds e ajustando alguns datatypes

--- migrations/20240117151749_init_db.up.sql
+++ migrations/20240117151749_init_db.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users
     name       varchar(120),
     type       varchar(20) check(type IN ('fisica', 'juridica')),
     documentId varchar(14),
-    avatar     varchar(255),
+    avatarUrl  varchar(255),
     email      varchar(128) not null,
     phone      varchar(12)  not null
 );
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS legal_persons
     id             uuid primary key default UUID(),
     userId         uuid REFERENCES users (id),
     phone          varchar(12) not null,
-    link           json,
-    openingHours   json        not null,
+    links          varchar(255),
+    openingHours   varchar(120)        not null,
     adoptionPolicy longtext    not null
 );
 
@@ -36,12 +36,31 @@ CREATE TABLE IF NOT EXISTS addresses
     latitude  float,
     longitute float
 );
+CREATE TABLE IF NOT EXISTS breeds
+(
+    id              uuid primary key default UUID(),
+    name            varchar(255)                      not null,
+    specie          varchar(255)                      not null,
+    size            varchar(20) check(size IN ('small', 'medium', 'large', 'giant')),
+    description     varchar(255),
+    height          varchar(10),
+    weight          varchar(10),
+    physicalChar varchar(255),
+    disposition     varchar(255),
+    idealFor        varchar(255),
+    fur             varchar(50),
+    imgUrl          varchar(255),
+    weather         varchar(255),
+    dressage        varchar(255),
+    orgId           varchar(25),
+    lifeExpectancy  varchar(30)
+);
 
 CREATE TABLE IF NOT EXISTS pets
 (
     id                  uuid primary key default UUID(),
     name                varchar(128)                      not null,
-    breedId             uuid                              not null,
+    breedId             uuid REFERENCES breeds(id)  not null,
     size                varchar(20) check(size IN ('small', 'medium', 'large', 'giant')),
     weight              decimal(3, 2)                     not null,
     adoptionDate        date                              not null,
@@ -67,24 +86,4 @@ CREATE TABLE IF NOT EXISTS vaccines
     name      varchar(128) not null,
     date      date         not null,
     doctorCRM varchar(15)  not null
-);
-
-CREATE TABLE IF NOT EXISTS breeds
-(
-    id              uuid primary key default UUID(),
-    name            varchar(255)                      not null,
-    specie          varchar(255)                      not null,
-    size            varchar(20) check(size IN ('small', 'medium', 'large', 'giant')),
-    description     varchar(255),
-    height          varchar(10),
-    weight          varchar(10),
-    physicalCharact varchar(255),
-    disposition     varchar(255),
-    idealFor        varchar(255),
-    fur             varchar(50),
-    imgUrl          varchar(255),
-    weather         varchar(255),
-    dressage        varchar(255),
-    orgId           varchar(25),
-    lifeExpectancy  varchar(30)
 );
