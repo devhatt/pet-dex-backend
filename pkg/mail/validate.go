7c95111913d6b160651c3f52dfa8a41bc8daa117
Lucas Campos <103159762+k1nha@users.noreply.github.com>
2024-02-08 18:55:13 -0300

    feat: create mail pkg (#65) (#84)
    * feat: create mail pkg
    
    * feat: setting up config
    
    * resolve request changes
    
    * interface mail

---
+++ pkg/mail/validate.go
@@ -0,0 +1,8 @@
+package mail
+
+import "regexp"
+
+func ValidateEmail(e string) bool {
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(e)
+}
