d7427ce9cfc99133ff4ecb0adeb5a2d3002a091d
Iasmim Cardoso <iasmimcardoso11@gmail.com>
2024-02-09 10:13:49 -0300

    fix: Mudando o tipo dos ID de String para ID

--- entity/pet.go
+++ entity/pet.go
@@ -1,16 +1,18 @@
 package entity
 
+import "pet-dex-backend/v2/pkg/entity"
+
 type Pet struct {
-	ID                  string  `json:"id"`
-	Name                string  `json:"name"`
-	BreedID             string  `json:"breed_id"`
-	Size                string  `json:"size"`
-	Weight              float64 `json:"weight"`
-	AdoptionDate        string  `json:"adoption_date"`
-	Birthdate           string  `json:"birthdate"`
-	Comorbidity         string  `json:"comorbidity"`
-	Tags                string  `json:"tags"`
-	Castrated           bool    `json:"castrated"`
-	AvailableToAdoption bool    `json:"available_to_adoption"`
-	UserID              string  `json:"user_id"`
+	ID                  entity.ID `json:"id"`
+	Name                string    `json:"name"`
+	BreedID             entity.ID `json:"breed_id"`
+	Size                string    `json:"size"`
+	Weight              float64   `json:"weight"`
+	AdoptionDate        string    `json:"adoption_date"`
+	Birthdate           string    `json:"birthdate"`
+	Comorbidity         string    `json:"comorbidity"`
+	Tags                string    `json:"tags"`
+	Castrated           bool      `json:"castrated"`
+	AvailableToAdoption bool      `json:"available_to_adoption"`
+	UserID              entity.ID `json:"user_id"`
 }
