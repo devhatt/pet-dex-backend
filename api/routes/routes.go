84c9f8b1b1799be30abbb83ea66a6990d39db144
David <davidambroziodev@gmail.com>
2024-03-20 22:11:56 -0300

    Refatorar FindByID do pet (#97)
    * refactor: update FindByID endpoint
    
    * test: Add tests for FindByID
    
    * refactor(test): update FindByID function tests

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -2,21 +2,19 @@ package routes
 
 import (
 	"pet-dex-backend/v2/api/controllers"
-	petcontroller "pet-dex-backend/v2/api/controllers/pet"
 
 	"github.com/go-chi/chi/v5"
 )
 
 type Controllers struct {
-	PetController     *controllers.PetController
-	FindPetController *petcontroller.FindPetController
+	PetController *controllers.PetController
 }
 
 func InitRoutes(controllers Controllers, c *chi.Mux) {
 
 	c.Route("/api", func(r chi.Router) {
 		r.Route("/pets", func(r chi.Router) {
-			r.Get("/{id}", controllers.FindPetController.FindPet)
+			r.Get("/{id}", controllers.PetController.FindPet)
 			r.Patch("/{petID}", controllers.PetController.Update)
 		})
 
