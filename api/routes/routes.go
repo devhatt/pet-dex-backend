78f960b13d63a3abb36fbf814fd9c66824455923
k1nha <lucascmpus@gmail.com>
2024-03-26 10:10:14 -0300

    fix: conflit

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,24 +1,15 @@
 package routes
 
 import (
-<<<<<<< HEAD
 	"pet-dex-backend/v2/api/controllers"
-	"fmt"
-	"encoding/json"
-=======
->>>>>>> 07b8768 (abstração da logica de geração de token para um usecase)
-	"net/http"
-	"pet-dex-backend/v2/api/controllers"
-	petcontroller "pet-dex-backend/v2/api/controllers/pet"
-	"pet-dex-backend/v2/api/middlewares"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 type Controllers struct {
-	PetController     *controllers.PetController
-	UserController    *controllers.UserController
+	PetController  *controllers.PetController
+	UserController *controllers.UserController
 }
 
 func InitRoutes(controllers Controllers, c *chi.Mux) {
@@ -29,7 +20,6 @@ func InitRoutes(controllers Controllers, c *chi.Mux) {
 		r.Route("/pets", func(r chi.Router) {
 			r.Get("/{id}", controllers.PetController.FindPet)
 
-			r.Get("/{id}", controllers.FindPetController.FindPet)
 			r.Patch("/{petID}", controllers.PetController.Update)
 		})
 
