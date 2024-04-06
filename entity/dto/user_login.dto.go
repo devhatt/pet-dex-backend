6c6b9ef8eb568954de9eefad0e587e67b0de22bb
Gabriel Trindade <gabriel.trindade@comuniki.me>
2024-04-06 17:11:15 -0300

    fix error return

--- entity/dto/user_login.dto.go
+++ entity/dto/user_login.dto.go
@@ -2,16 +2,14 @@ package dto
 
 import "errors"
 
-
-
 type UserLoginDto struct {
-	Email  string `json:"Email"`
+	Email    string `json:"Email"`
 	Password string `json:"password"`
 }
 
 func (u *UserLoginDto) Validate() error {
 	if u.Email == "" {
-		return errors.New("name cannot be empty")
+		return errors.New("email cannot be empty")
 	}
 	if u.Password == "" {
 		return errors.New("password cannot be empty")
