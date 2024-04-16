83285ba349249ec9ad407a84fa8a5d78407815b2
Clerivaldo <clerivaldoj@gmail.com>
2024-04-16 13:07:33 -0300

    chore: Adjust ong usecase

--- usecase/ong.go
+++ usecase/ong.go
@@ -7,20 +7,49 @@ import (
 )
 
 type OngUsecase struct {
-	repo interfaces.OngRepository
+	repo   interfaces.OngRepository
+	hasher interfaces.Hasher
 }
 
-func NewOngUseCase(repo interfaces.OngRepository) *OngUsecase {
-	return &OngUsecase{repo: repo}
+func NewOngUseCase(repo interfaces.OngRepository, hasher interfaces.Hasher) *OngUsecase {
+	return &OngUsecase{
+		repo:   repo,
+		hasher: hasher,
+	}
 }
 
 func (o *OngUsecase) Save(ong *entity.Ong) error {
-	err := o.repo.Save(ong)
+	user := entity.NewUser(ong.Name, ong.Type, ong.Document, ong.AvatarURL, ong.Email, ong.Phone, ong.Pass, ong.Address.City, ong.Address.State, ong.CreationDate)
+	hashedPass, err := o.hasher.Hash(ong.Pass)
 
 	if err != nil {
-		fmt.Println(fmt.Errorf("error saving ong: %w", err))
+		fmt.Println(fmt.Errorf("#OngUsecase.Hash error: %w", err))
+		return err
+	}
+
+	user.Pass = hashedPass
+
+	err = o.repo.SaveUser(user)
+
+	if err != nil {
+		fmt.Println(fmt.Errorf("#OngUseCase.SaveUser error: %w", err))
+		return err
+	}
+
+	err = o.repo.SaveAddress(&user.Adresses)
+
+	if err != nil {
+		fmt.Println(fmt.Errorf("#OngUseCase.SaveAddress error: %w", err))
+		return err
+	}
+
+	err = o.repo.Save(ong, user.ID)
+
+	if err != nil {
+		fmt.Println(fmt.Errorf("#OngUseCase.Save error: %w", err))
 		return err
 	}
 
 	return nil
+
 }
