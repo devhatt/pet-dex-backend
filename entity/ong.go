1a501a24c79d85dfbe53659b2c086ea748322335
Clerivaldo <clerivaldoj@gmail.com>
2024-04-18 18:01:29 -0300

    chore: Adjust ong entity

--- entity/ong.go
+++ entity/ong.go
@@ -9,18 +9,10 @@ import (
 
 type Ong struct {
 	ID             uniqueEntityId.ID `json:"id" db:"id"`
-	Name           string            `json:"nome" db:"name"`
-	Type           string            `json:"type"`
-	Document       string            `json:"cnpj" db:"document"`
-	AvatarURL      string            `json:"imagem" db:"avatarUrl"`
-	Email          string            `json:"email"`
-	Pass           string            `json:"senha" db:"pass"`
-	Phone          string            `json:"telefone" db:"phone"`
-	Address        Address           `json:"localizacao"`
-	OpeningHours   string            `json:"horario_funcionamento"`
-	AdoptionPolicy string            `json:"politica_adocao"`
-	CreationDate   *time.Time        `json:"data_criacao"`
-	SocialMedia    *json.RawMessage  `json:"redes_sociais"`
+	User           User
+	OpeningHours   string           `json:"horario_funcionamento"`
+	AdoptionPolicy string           `json:"politica_adocao"`
+	SocialMedia    *json.RawMessage `json:"redes_sociais"`
 
 	CreatedAt *time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt *time.Time `json:"updatedAt" db:"updated_at"`
@@ -29,7 +21,7 @@ type Ong struct {
 func NewOng(name, uType, document, avatar_url, email, phone, pass, city, state, openingHours, adoptionPolicy string, creationDate *time.Time, socialMedia json.RawMessage) *Ong {
 	ongId := uniqueEntityId.NewID()
 
-	address := NewAddress(ongId, city, state)
+	user := NewUser(name, uType, document, avatar_url, email, phone, pass, city, state, creationDate)
 
 	var socials *json.RawMessage
 	err := json.Unmarshal(socialMedia, &socials)
@@ -39,16 +31,8 @@ func NewOng(name, uType, document, avatar_url, email, phone, pass, city, state, 
 
 	return &Ong{
 		ID:             ongId,
-		Name:           name,
-		Type:           uType,
-		CreationDate:   creationDate,
-		Document:       document,
-		AvatarURL:      avatar_url,
-		Email:          email,
-		Pass:           pass,
-		Phone:          phone,
+		User:           *user,
 		SocialMedia:    socials,
-		Address:        address,
 		OpeningHours:   openingHours,
 		AdoptionPolicy: adoptionPolicy,
 	}
