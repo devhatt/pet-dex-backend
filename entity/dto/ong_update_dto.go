9ae6507f7936237bb1d8d035c6d1d7efd5d680a6
Clerivaldo <clerivaldoj@gmail.com>
2024-05-04 17:50:47 -0300

    fix: Removed comments

--- entity/dto/ong_update_dto.go
+++ entity/dto/ong_update_dto.go
@@ -5,10 +5,6 @@ import (
 )
 
 type OngUpdateDto struct {
-	/* Name           string           `json:"name" db:"name"`
-	Document       string           `json:"document" db:"document"`
-	AvatarURL      string           `json:"avatar_url" db:"avatarUrl"`
-	Email          string           `json:"email" db:"email"`*/
 	Phone          string `json:"phone" db:"phone"`
 	User           UserUpdateDto
 	OpeningHours   string           `json:"openingHours"`
