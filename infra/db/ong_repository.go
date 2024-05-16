09f50020aba14f009c2cdd65c2eded75a4fbddf6
Clerivaldo <clerivaldoj@gmail.com>
2024-05-15 21:20:58 -0300

    feat: add find by id to ong repo

--- infra/db/ong_repository.go
+++ infra/db/ong_repository.go
@@ -1,10 +1,12 @@
 package db
 
 import (
+	"database/sql"
 	"fmt"
 	"pet-dex-backend/v2/entity"
 	"pet-dex-backend/v2/infra/config"
 	"pet-dex-backend/v2/interfaces"
+	"pet-dex-backend/v2/pkg/uniqueEntityId"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -32,4 +34,52 @@ func (or *OngRepository) Save(ong *entity.Ong) error {
 	}
 
 	return nil
+}
+
+func (or *OngRepository) FindByID(ID uniqueEntityId.ID) (*entity.Ong, error) {
+	row, err := or.dbconnection.Query(`
+	SELECT
+		l.id,
+		l.userId,
+		l.links,
+		l.openingHours,
+		l.adoptionPolicy
+	FROM
+		legal_persons l
+	WHERE
+		l.id = ?`,
+		ID,
+	)
+	if err != nil {
+		logger.Error("error on ong repository: ", err)
+		err = fmt.Errorf("error retrieving ong %d: %w", ID, err)
+		return nil, err
+	}
+	defer row.Close()
+
+	if !row.Next() {
+		return nil, sql.ErrNoRows
+	}
+
+	var ong entity.Ong
+
+	err = row.Scan(
+		&ong.ID,
+		&ong.UserID,
+		&ong.OpeningHours,
+		&ong.AdoptionPolicy,
+		&ong.Links,
+	)
+	if err != nil {
+		logger.Error("error on ong repository: ", err)
+		err = fmt.Errorf("error retrieving ong %d: %w", ID, err)
+		return nil, err
+	}
+
+	if err := row.Err(); err != nil {
+		logger.Error("error on ong repository: ", err)
+		return nil, fmt.Errorf("error iterating over ong rows: %w", err)
+	}
+
+	return &ong, err
 }
--- interfaces/ong_repository.go
+++ interfaces/ong_repository.go
@@ -2,8 +2,10 @@ package interfaces
 
 import (
 	"pet-dex-backend/v2/entity"
+	"pet-dex-backend/v2/pkg/uniqueEntityId"
 )
 
 type OngRepository interface {
 	Save(ong *entity.Ong) error
+	FindByID(ID uniqueEntityId.ID) (*entity.Ong, error)
 }
