b21d0ed3a79de2f01893ba41d5db219ab25e4393
Matheus Melo <117240363+matheus-webp@users.noreply.github.com>
2024-08-09 14:55:39 -0300

    Criar Delete de ONG - Issue 113 (#161)
    * feat: add deletedAt field legal_persons
    
    * feat: add delete ong repository
    
    * feat: add delete ong usecase
    
    * feat: add delete dto
    
    * feat: add delete ong controller
    
    * feat: add delete ong usecase test
    
    * fix: fix tests for OngUsecase's Delete method
    
    * feat: add delete ong route

--- entity/ong.go
+++ entity/ong.go
@@ -19,6 +19,7 @@ type Ong struct {
 
 	CreatedAt *time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt *time.Time `json:"updatedAt" db:"updated_at"`
+	DeletedAt *time.Time `json:"deletedAt" db:"deleted_at"`
 }
 
 func NewOng(ong dto.OngInsertDto) *Ong {
---
+++ usecase/ong_test.go
@@ -0,0 +1,43 @@
+package usecase
+
+import (
+	"pet-dex-backend/v2/entity"
+	mockInterfaces "pet-dex-backend/v2/mocks/pet-dex-backend/v2/interfaces"
+	"pet-dex-backend/v2/pkg/uniqueEntityId"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOngDelete(t *testing.T) {
+	tcases := map[string]struct {
+		repo         *mockInterfaces.MockOngRepository
+		inputID      uniqueEntityId.ID
+		findByIDResp *entity.Ong
+		findByIDErr  error
+		deleteResp   error
+		expectOutput error
+	}{
+		"success": {
+			repo:         mockInterfaces.NewMockOngRepository(t),
+			inputID:      uniqueEntityId.NewID(),
+			findByIDResp: &entity.Ong{},
+			findByIDErr:  nil,
+			deleteResp:   nil,
+			expectOutput: nil,
+		},
+	}
+
+	for name, tcase := range tcases {
+		t.Run(name, func(t *testing.T) {
+			tcase.repo.On("FindByID", tcase.inputID).Return(nil, tcase.expectOutput)
+			tcase.repo.On("Delete", tcase.inputID).Return(tcase.expectOutput)
+
+			usecase := NewOngUseCase(tcase.repo, nil, nil)
+			err := usecase.Delete(tcase.inputID)
+
+			assert.Equal(t, tcase.expectOutput, err, "expected error mismatch")
+			tcase.repo.AssertExpectations(t)
+		})
+	}
+}
