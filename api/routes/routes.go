d9006dde1dd97dbe802a02dd93ced2ce347a015c
juliaam <juliademoraess@gmail.com>
2024-09-06 18:11:22 -0300

    Revert "feat: my pets swagger"
    This reverts commit 183a114da604aa38c8db591090b9c632572ae4cc.

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -42,7 +42,7 @@ func InitRoutes(controllers Controllers, c *chi.Mux) {
 			})
 
 			private.Route("/user", func(r chi.Router) {
-				r.Get("/{userID}/my-pets", controllers.PetController.ListUserPets)
+				r.Get("/{id}/my-pets", controllers.PetController.ListUserPets)
 				r.Patch("/{userID}/pets/{petID}", controllers.PetController.Update)
 				r.Patch("/{id}", controllers.UserController.Update)
 				r.Get("/{id}", controllers.UserController.FindByID)
