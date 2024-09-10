24ac45fd591cf9a6347e9f65b449b102b7c9e82f
Edilton Oliveira <86030948+EdiltonOliveira@users.noreply.github.com>
2024-09-10 19:38:28 -0300

    feat: documentação rota /api/pets/breeds/ GET # issue-177 (#198)
    * feat: documentação rota /api/pets/breeds/ GET # issue-177
    
    * docs: corrigi descrição do endpoint da API.
    
    * docs: atualizações da docs para correção de inglês.

--- api/controllers/breed.go
+++ api/controllers/breed.go
@@ -22,6 +22,15 @@ func NewBreedController(usecase *usecase.BreedUseCase) *BreedController {
 	}
 }
 
+// List retrieves breeds information for all pets.
+// @Summary View list of all Breeds
+// @Description Retrieves list of all pet breeds
+// @Tags Pet
+// @Produce json
+// @Success 200 {object} dto.BreedList
+// @Failure 400
+// @Failure 500
+// @Router /pets/breeds/ [get]
 func (cntrl *BreedController) List(responseWriter http.ResponseWriter, request *http.Request) {
 	breeds, err := cntrl.Usecase.List()
 	if err != nil {
